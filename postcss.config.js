c72593779299b2ca7380ea79f041048698d78390
Alan Flood (Digital) <al952368@c02x47w7jgh6.wipro.com>
2019-09-25 16:25:41 +0100

    Bump dependencies and fix tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,5 @@
 var tailwindcss = require("tailwindcss");
 var postcssPresetEnv = require("postcss-preset-env");
 module.exports = {
-  plugins: [tailwindcss("./tailwind.js"), postcssPresetEnv()]
+  plugins: [tailwindcss(), postcssPresetEnv()]
 };
--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,24 +1,50 @@
 -- Elm stuff should go in here!
+
+
+module Main exposing (Msg(..), main, update, view)
+
 import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html exposing (..)
+import Html.Events exposing (..)
 
-main =
-  Browser.sandbox { init = 0, update = update, view = view }
 
-type Msg = Increment | Decrement
+type alias Model =
+    Int
 
-update msg model =
-  case msg of
-    Increment ->
-      model + 1
 
-    Decrement ->
-      model - 1
+initialModel : Model
+initialModel =
+    0
 
+
+view : Model -> Html Msg
 view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
+    div []
+        [ button [ onClick Decrement ] [ text "-" ]
+        , text (String.fromInt model)
+        , button [ onClick Increment ] [ text "+" ]
+        ]
+
+
+type Msg
+    = Increment
+    | Decrement
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Increment ->
+            model + 1
+
+        Decrement ->
+            model - 1
+
+
+main : Program () Model Msg
+main =
+    Browser.sandbox
+        { init = initialModel
+        , view = view
+        , update = update
+        }
